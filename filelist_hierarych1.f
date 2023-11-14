169dcefb165cb7311ac8699358318752bd1c2c8a
Shaosss <42096123+shaokaiheng@users.noreply.github.com>
2023-11-14 17:21:23 +0800

    Update filelist_hierarych1.f

--- filelist_hierarych1.f
+++ filelist_hierarych1.f
@@ -1,4 +1,4 @@
 /module_hierarchy/module_1_1.v
 /module_hierarchy/module_2_2.v
 /module_hierarchy/module_a.v
--f C:/Users/shaok/PycharmProjects/pythonProject/check_filelist/filelist_hierarych_2.f
+-f ./filelist_hierarych_2.f
